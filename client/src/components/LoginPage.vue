f9dce3988e312025e718d5aee07f7786d92dd697
John <ashton.basile@gmail.com>
2024-01-22 15:38:54 +0000

    converted the LoginPage.vue from the options API to the composition API

--- client/src/components/LoginPage.vue
+++ client/src/components/LoginPage.vue
@@ -115,106 +115,99 @@
   </div>
 </template>
 
-<!-- <script setup lang="ts"></script> -->
-
-<script lang="ts">
+<script setup lang="ts">
 import axios from 'axios';
+import { defineEmits, ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const emit = defineEmits(['emitUser']);
+const router = useRouter();
 const url = '/api/users/';
 
-export default {
-  name: 'LoginPage',
-  components: {},
-  props: {},
-  data() {
-    return {
-      userNameInput: '',
-      passwordInput: '',
-      userNameInputNew: '',
-      passwordInputNew: '',
-      showLogin: true,
-      boxAcknowledged: false,
-    };
-  },
-  methods: {
-    notificationButtonClicked() {
-      this.boxAcknowledged = true;
-    },
-    activateShowLogin() {
-      this.showLogin = true;
-    },
-    deActivateShowLogin() {
-      this.showLogin = false;
-    },
-    async logIn() {
-      if (this.userNameInput == '' || this.passwordInput == '') {
-        this.showSnackBar('snackbar4');
-        this.clearLogInInputs();
-        return;
-      }
-      const responseFromUsers = await axios.post(url + '/login', {
-        userName: this.userNameInput,
-        userPassword: this.passwordInput,
-      });
-      const user = responseFromUsers.data;
+const userNameInput = ref('');
+const passwordInput = ref('');
+const userNameInputNew = ref('');
+const passwordInputNew = ref('');
+const showLogin = ref(true);
+const boxAcknowledged = ref(false);
 
-      if (responseFromUsers.status == 205) {
-        this.showSnackBar('snackbar1');
-        this.clearLogInInputs();
-        return;
-      }
-      if (responseFromUsers.status == 202) {
-        this.showSnackBar('snackbar3');
-        this.clearLogInInputs();
-        return;
-      }
+const notificationButtonClicked = () => {
+  boxAcknowledged.value = true;
+};
+const activateShowLogin = () => {
+  showLogin.value = true;
+};
+const deActivateShowLogin = () => {
+  showLogin.value = false;
+};
+const logIn = async function () {
+  if (userNameInput.value == '' || passwordInput.value == '') {
+    showSnackBar('snackbar4');
+    clearLogInInputs();
+    return;
+  }
+  const responseFromUsers = await axios.post(url + '/login', {
+    userName: userNameInput.value,
+    userPassword: passwordInput.value,
+  });
+  const user = responseFromUsers.data;
 
-      this.$emit('emitUser', user);
-      this.clearLogInInputs();
-      this.$router.push({ path: '/welcome/' });
-    },
-    async signUp() {
-      if (this.userNameInputNew == '' || this.passwordInputNew == '') {
-        this.showSnackBar('snackbar4');
-        this.clearSignUpInputs();
-        return;
-      }
-      const response = await axios.post(url, {
-        userName: this.userNameInputNew,
-        userPassword: this.passwordInputNew,
-      });
-      if (response.status == 205) {
-        this.showSnackBar('snackbar2');
-        this.clearSignUpInputs();
-        return;
-      }
-      const userObj = response.data;
-      this.clearSignUpInputs();
-      this.$emit('emitUser', userObj);
-      this.$router.push({ path: '/welcome/' });
-    },
-    showSnackBar(snackBarNum: string) {
-      // Get the snackbar DIV
-      let snackBarElement = document.getElementById(snackBarNum);
-      // Add the "show" class to DIV
-      if (snackBarElement != null) {
-        snackBarElement.classList.add('show');
-      }
-      // After 3 seconds, remove the show class from DIV
-      setTimeout(function () {
-        if (snackBarElement != null) {
-          snackBarElement.classList.remove('show');
-        }
-      }, 3000);
-    },
-    clearLogInInputs() {
-      this.userNameInput = '';
-      this.passwordInput = '';
-    },
-    clearSignUpInputs() {
-      this.userNameInputNew = '';
-      this.passwordInputNew = '';
-    },
-  },
+  if (responseFromUsers.status == 205) {
+    showSnackBar('snackbar1');
+    clearLogInInputs();
+    return;
+  }
+  if (responseFromUsers.status == 202) {
+    showSnackBar('snackbar3');
+    clearLogInInputs();
+    return;
+  }
+
+  emit('emitUser', user);
+  clearLogInInputs();
+  router.push({ path: '/welcome/' });
+};
+const signUp = async function () {
+  if (userNameInputNew.value == '' || passwordInputNew.value == '') {
+    showSnackBar('snackbar4');
+    clearSignUpInputs();
+    return;
+  }
+  const response = await axios.post(url, {
+    userName: userNameInputNew.value,
+    userPassword: passwordInputNew.value,
+  });
+  if (response.status == 205) {
+    showSnackBar('snackbar2');
+    clearSignUpInputs();
+    return;
+  }
+  const userObj = response.data;
+  clearSignUpInputs();
+  emit('emitUser', userObj);
+  router.push({ path: '/welcome/' });
+};
+const showSnackBar = (snackBarNum: string) => {
+  // Get the snackbar DIV
+  let snackBarElement = document.getElementById(snackBarNum);
+  // Add the "show" class to DIV
+  if (snackBarElement != null) {
+    snackBarElement.classList.add('show');
+  }
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function () {
+    if (snackBarElement != null) {
+      snackBarElement.classList.remove('show');
+    }
+  }, 3000);
+};
+const clearLogInInputs = () => {
+  userNameInput.value = '';
+  passwordInput.value = '';
+};
+const clearSignUpInputs = () => {
+  userNameInputNew.value = '';
+  passwordInputNew.value = '';
 };
 </script>
 
